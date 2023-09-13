a1fbb770a2d5a7484942b8e2bf29713617acfa4f
Arthurito <cristiantotriarthurito@gmail.com>
2023-09-13 14:12:28 +0700

    login & register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name', 255);
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
+            $table->bigInteger('phone')->nullable();
+            $table->string('location')->nullable();
+            $table->string('about_me')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
