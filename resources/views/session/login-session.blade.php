2fe49bed9f70abd0cb9b64b08b924c3ac416f496
Arthurito <cristiantotriarthurito@gmail.com>
2023-09-15 14:07:50 +0700

    Login

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -1,67 +1,62 @@
 @extends('layouts.user_type.guest')
 
 @section('content')
-
-  <main class="main-content  mt-0">
+  <main class="main-content mt-0 vh-100" style="background-image: url(../assets/img/bgLogin.png); background-size: cover; background-position: center; ">
     <section>
       <div class="page-header min-vh-75">
         <div class="container">
-          <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
-              <div class="card card-plain mt-8">
-                <div class="card-header pb-0 text-left bg-transparent">
-                  <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
-                  <p class="mb-0">Create a new acount<br></p>
-                  <p class="mb-0">OR Sign in with these credentials:</p>
-                  <p class="mb-0">Email <b>admin@softui.com</b></p>
-                  <p class="mb-0">Password <b>secret</b></p>
-                </div>
+
+            <div class="card m-auto mt-8 p-5" style="width: 40%">
                 <div class="card-body">
-                  <form role="form" method="POST" action="/session">
-                    @csrf
-                    <label>Email</label>
-                    <div class="mb-3">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="admin@softui.com" aria-label="Email" aria-describedby="email-addon">
-                      @error('email')
-                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                      @enderror
-                    </div>
-                    <label>Password</label>
-                    <div class="mb-3">
-                      <input type="password" class="form-control" name="password" id="password" placeholder="Password" value="secret" aria-label="Password" aria-describedby="password-addon">
-                      @error('password')
-                        <p class="text-danger text-xs mt-2">{{ $message }}</p>
-                      @enderror
-                    </div>
-                    <div class="form-check form-switch">
-                      <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
-                      <label class="form-check-label" for="rememberMe">Remember me</label>
-                    </div>
-                    <div class="text-center">
-                      <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
-                    </div>
-                  </form>
+                    <h3 class="card-title text-dark">SIGN TO YOUR ACCOUNT</h3>
+                    <form role="form" method="POST" action="/session">
+                        @csrf
+                        <div class="wave-group my-4">
+                            <i class="fas fa-envelope"></i> <!-- Icon -->
+                            <input required="" type="email" class="input" name="email" id="email">
+                            @error('email')
+                            <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                            <span class="bar"></span>
+                            <label class="label">
+                                <span class="label-char" style="--index: 0">E</span>
+                                <span class="label-char" style="--index: 1">m</span>
+                                <span class="label-char" style="--index: 2">a</span>
+                                <span class="label-char" style="--index: 3">i</span>
+                                <span class="label-char" style="--index: 4">l</span>
+                            </label>
+                        </div>
+
+                        <div class="wave-group my-4">
+                            <i class="fas fa-envelope"></i> <!-- Icon -->
+                            <input required="" type="password" class="input" name="password" id="password">
+                            @error('password')
+                            <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                            <span class="bar"></span>
+                            <label class="label">
+                                <span class="label-char" style="--index: 0">P</span>
+                                <span class="label-char" style="--index: 1">a</span>
+                                <span class="label-char" style="--index: 2">s</span>
+                                <span class="label-char" style="--index: 3">s</span>
+                                <span class="label-char" style="--index: 4">w</span>
+                                <span class="label-char" style="--index: 5">o</span>
+                                <span class="label-char" style="--index: 6">r</span>
+                                <span class="label-char" style="--index: 7">d</span>
+                            </label>
+                        </div>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn w-100 mt-4 mb-0 text-light" style="background-color: #CE3534; font-weight: bolder">L o g i n</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                <small class="text-muted">Forgot you password? Reset you password 
-                  <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
-                </small>
-                  <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
-                  </p>
-                </div>
-              </div>
             </div>
-            <div class="col-md-6">
-              <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
-                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../assets/img/curved-images/curved6.jpg')"></div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </section>
   </main>
-
 @endsection
